a7b8dc9d266e9b39e9c6764cc713571d7cd821aa
Hamzadevolperweb <mh1729574@gmail.com>
2026-04-30 01:03:45 +0500

    Fix AnimatePresence import

--- client/src/components/ui/Footer.jsx
+++ client/src/components/ui/Footer.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function Footer() {
   const [email, setEmail] = useState('');
@@ -191,3 +191,4 @@ function Footer() {
 }
 
 export default Footer;
+
